501d5f868f7b4b61527ae6998420b34169e0c7f3
Jeff Eldredge <jdeldre@g.ucla.edu>
2024-05-21 09:49:54 -0700

    Added inviscid version

--- src/baseflows.jl
+++ src/baseflows.jl
@@ -1,6 +1,8 @@
 # Base flow functions
 
-for bflow in (:poiseuille, :couette)
+const TANHFACTOR=7.0
+
+for bflow in (:poiseuille, :couette, :sharpmixing, :tanhmixing)
     Ufcn = Symbol("_U_",bflow)
     dUfcn = Symbol("_dU_",bflow)
     d2Ufcn = Symbol("_d2U_",bflow)
@@ -8,6 +10,13 @@ for bflow in (:poiseuille, :couette)
         U = $Ufcn.(y)
         dU = $dUfcn.(y)
         d2U = $d2Ufcn.(y)
+        return U, dU, d2U
+    end
+
+    @eval function $bflow(y::Float64)
+        U = $Ufcn(y)
+        dU = $dUfcn(y)
+        d2U = $d2Ufcn(y)
         return U, dU, d2U
     end
 end
@@ -20,3 +29,12 @@ _d2U_poiseuille(y) = -2
 _U_couette(y) = y
 _dU_couette(y) = 1
 _d2U_couette(y) = 0
+
+_U_sharpmixing(y) = y >= 0 ? 1 : -1
+_dU_sharpmixing(y) = 0
+_d2U_sharpmixing(y) = 0
+
+_U_tanhmixing(y) = 0.5*(1+tanh(TANHFACTOR*y))
+_dU_tanhmixing(y) = 0.5*TANHFACTOR*sech(TANHFACTOR*y)^2
+_d2U_tanhmixing(y) = -TANHFACTOR^2*tanh(TANHFACTOR*y)*sech(TANHFACTOR*y)^2
+
--- src/osmatrices.jl
+++ src/osmatrices.jl
@@ -71,6 +71,58 @@ function os_matrices(α::Real,β::Real,Re::Real; N::Integer=200,baseflow=:poiseu
 end
 
 """
+    os_matrices(α,β[;baseflow=:tanhmixing, N=200]) -> OSMatrix
+
+Returns the Rayleigh equation matrices for a given base flow (between walls at y = +1/-1), for
+streamwise wavenumber `α` and spanwise wavenumber `β`.
+The default base flow is a tanh mixing layer, 0.5*(1+tanh(7y)), but this can be changed to `:couette` for
+Couette flow. Optional argument `N` specifies the number of Chebyshev modes to use.
+It defaults to 200.
+"""
+function os_matrices(α::Real,β::Real; N::Integer=200,baseflow=:tanhmixing)
+    C = Cheb(N)
+
+    @unpack y, D0, D1, D2, D3, D4 = C
+
+    ak2 = α^2 + β^2
+    Nos = N+1
+    Nsq = N+1
+
+    # Base flow and its derivative
+    U, dU, d2U  = eval(baseflow)(y)    
+
+    Aos, Aoq, Asq = zeros(ComplexF64,Nos,Nos), zeros(ComplexF64,Nos,Nos), zeros(ComplexF64,Nos,Nos)
+    Bos, Bsq = zeros(ComplexF64,Nos,Nos), zeros(ComplexF64,Nos,Nos)
+    er = -200.0*im # This ensures that spurious eigenvalues from bc's are far away
+
+    # Form the Orr-Sommerfeld operators Aos and Bos
+    Bos .= D2 - ak2*D0
+    Aos .= α*Diagonal(U)*Bos - α*Diagonal(d2U)*D0
+    Aos[1,:] = er*D0[1,:] # v(1) = 0
+    Aos[Nos,:] = er*D0[Nos,:] # v(-1) = 0
+
+    Bos[1,:] .= D0[1,:]
+    Bos[Nos,:] .= D0[Nos,:]
+
+    # Form the Squire operators Asq and Bsq
+    Asq .= α*Diagonal(U)*D0
+    Bsq .= D0
+    #Asq[1,:] = er*D0[1,:]  # eta(1) = 0
+    #Asq[Nsq,:] = er*D0[Nsq,:] # eta(-1) = 0
+
+    # Form the coupling operator from OS to Squire
+    Aoq .= β*Diagonal(dU)*D0 
+    #Aoq[1,:] = zeros(1,Nos)
+    #Aoq[Nsq,:] = zeros(1,Nos)
+
+    # Form the energy weight matrix
+    M = energy_matrix(N+1,N+1,ak2)
+
+    return OSMatrix{N}(α, β, ak2, C, Aos, Bos, Asq, Bsq, Aoq, M)
+    
+end
+
+"""
     zero_q(d::OSMatrix)
 
 Return a disturbance vector of zeros for O-S system `d`.
@@ -131,14 +183,14 @@ function _os_eigen_raw(d::OSMatrix{N},minval,maxval,normalize,categorize,::Val{:
     λ, X = eigen(d.Aos,d.Bos,sortby=(y -> -imag(y)))
     _normalize_eigenvectors!(X,d.M[1:N+1,1:N+1],Val(normalize))
     λu,Xu = _limit_eigenvalues(λ,X,minval,maxval)
-    return OSEigen(collect(1:length(λ)),Integer[],λu,Xu)
+    return OSEigen(collect(1:length(λu)),Integer[],λu,Xu)
 end
 
 function _os_eigen_raw(d::OSMatrix{N},minval,maxval,normalize,categorize,::Val{:sq}) where N
     λ, X = eigen(d.Asq,d.Bsq,sortby=(y -> -imag(y)))
     _normalize_eigenvectors!(X,d.M[N+2:2N+2,N+2:2N+2],Val(normalize))
     λu,Xu = _limit_eigenvalues(λ,X,minval,maxval)
-    return OSEigen(Integer[],collect(1:length(λ)),λu,Xu)
+    return OSEigen(Integer[],collect(1:length(λu)),λu,Xu)
 end
 
 function _os_eigen_raw(d::OSMatrix{N},minval,maxval,normalize,categorize,::Val{:full}) where N
